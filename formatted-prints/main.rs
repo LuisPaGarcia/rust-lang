7d7f6c655c16f44d71932a16912395cf77ed754b
LuisPa Garcia <diganluispa@gmail.com>
2021-12-31 12:09:44 -0600

    feat: Learning custom print

---
+++ formatted-prints/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+  // Print a value by using {} 
+  println!("{} days", 31);
+
+  // Using index value to print a specific value 
+  println!("{0} is the first, {1} is the second, and {2} is the third.", "ale", "sofi", "diego");
+
+  // Using naming convention to print by name
+  println!("{name} is {verb}", name="LuisPa", verb="tall");
+
+  // Special format by using `:`
+  println!("{} of {:b} people know binary, the other half  doesn't", 1, 2);
+
+  
+
+
+}
